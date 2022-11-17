1eb49344e601330e2535e28db90060935207faa5
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-11-17 14:59:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  const [stat, setStat] = useState({})
+  useEffect(() => {
+    fetch("https://statsapi.web.nhl.com/api/v1/people/8480069/stats?stats=statsSingleSeason")
+    .then(response => {
+      if (!response.ok) {
+        console.log("error")
+      } else {
+        return response.json()
+      }
+    })
+    .then(data => {
+      setStat(data.stats[0].splits[0].stat)
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <p>Pts:{stat.points}</p>
+      {console.log(stat)}
+    </main>
+  )
 }
 
 export default App;
